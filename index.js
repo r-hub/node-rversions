b1f42b566c2f87d6cdaaecb3bf61bb637de79024
Gábor Csárdi <csardi.gabor@gmail.com>
2020-03-04 14:54:08 +0000

    Add r_release_tarball

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
-const r_versions      = require('./lib/r-versions');
-const r_release       = require('./lib/r-release');
-const r_oldrel        = require('./lib/r-oldrel');
-const r_release_macos = require('./lib/r-release-macos');
+const r_versions        = require('./lib/r-versions');
+const r_release         = require('./lib/r-release');
+const r_oldrel          = require('./lib/r-oldrel');
+const r_release_macos   = require('./lib/r-release-macos');
+const r_release_tarball = require('./lib/r-release-tarball');
 
 module.exports = {
-    r_versions:      r_versions,
-    r_release:       r_release,
-    r_oldrel:        r_oldrel,
-    r_release_macos: r_release_macos
+    r_versions:        r_versions,
+    r_release:         r_release,
+    r_oldrel:          r_oldrel,
+    r_release_macos:   r_release_macos,
+    r_release_tarball: r_release_tarball
 };
