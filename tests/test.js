6e28a2711c47b95feffc1bb0177a4c03074a942e
Gábor Csárdi <csardi.gabor@gmail.com>
2020-03-04 23:38:06 +0000

    Caching improvements
    * fix a bug in the 'r_versions' cache.
    * always set the cache if a new query was made.
    * better tests for caching.

--- tests/test.js
+++ tests/test.js
@@ -5,68 +5,84 @@ const get_nick = require('../lib/get-nick');
 const mycache = require('../lib/cache');
 
 test('r_versions', async t => {
-    const result = await me.r_versions();
-    t.is(result[0].version, '0.60');
-    t.is(result[0].nickname, null);
+    // We run it twive, the second result comes from the cache
+    for (let i = 0; i < 2; i++) {
+        const result = await me.r_versions();
+        t.is(result[0].version, '0.60');
+        t.is(result[0].nickname, null);
 
-    const versions = result.map(function(x) { return x.version; })
-    t.true(versions.indexOf('3.6.3') > -1);
-    t.is(result[versions.indexOf('2.14.0')].nickname, 'Great Pumpkin');
+        const versions = result.map(function(x) { return x.version; })
+        t.true(versions.indexOf('3.6.3') > -1);
+        t.is(result[versions.indexOf('2.14.0')].nickname, 'Great Pumpkin');
 
-    const dates = result.map(function(x) { return Date.parse(x.date); })
-    t.pass()
+        const dates = result.map(function(x) { return Date.parse(x.date); })
+        t.pass()
+    }
 });
 
 test('r_release', async t => {
-    const result = await me.r_release();
-    t.deepEqual(Object.keys(result).sort(), ['date', 'nickname', 'version'])
-    t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
-    Date.parse(result.date);
-    t.pass()
-    t.is(result.nickname, await get_nick(result.version));
+    for (let i = 0; i < 2; i++) {
+        const result = await me.r_release();
+        t.deepEqual(
+            Object.keys(result).sort(),
+            ['date', 'nickname', 'version']
+        );
+        t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
+        Date.parse(result.date);
+        t.pass()
+        t.is(result.nickname, await get_nick(result.version));
+    }
 });
 
 test('r_oldrel', async t => {
-    const vers = await Promise.all([me.r_release(), me.r_oldrel()]);
-    const rm = vers[0].version.replace(/^[0-9]+\.([0-9]+).*$/, '$1');
-    const om = vers[1].version.replace(/^[0-9]+\.([0-9]+).*$/, '$1');
-    t.is(Number(rm) - 1, Number(om));
+    for (let i = 0; i < 2; i++) {
+        const vers = await Promise.all([me.r_release(), me.r_oldrel()]);
+        const rm = vers[0].version.replace(/^[0-9]+\.([0-9]+).*$/, '$1');
+        const om = vers[1].version.replace(/^[0-9]+\.([0-9]+).*$/, '$1');
+        t.is(Number(rm) - 1, Number(om));
+    }
 });
 
 test('r_release_macos', async t => {
-    const result = await me.r_release_macos();
-    t.deepEqual(
-        Object.keys(result).sort(),
-        ['URL', 'date', 'nickname', 'version']
-    )
-    t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
-    Date.parse(result.date);
-    t.pass()
-    t.is(result.nickname, await get_nick(result.version));
+    for (let i = 0; i < 2; i++) {
+        const result = await me.r_release_macos();
+        t.deepEqual(
+            Object.keys(result).sort(),
+            ['URL', 'date', 'nickname', 'version']
+        )
+        t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
+        Date.parse(result.date);
+        t.pass()
+        t.is(result.nickname, await get_nick(result.version));
+    }
 });
 
 test('r_release_tarball', async t => {
-    const result = await me.r_release_tarball();
-    t.deepEqual(
-        Object.keys(result).sort(),
-        ['URL', 'date', 'nickname', 'version']
-    )
-    t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
-    Date.parse(result.date);
-    t.pass()
-    t.is(result.nickname, await get_nick(result.version));
+    for (let i = 0; i < 2; i++) {
+        const result = await me.r_release_tarball();
+        t.deepEqual(
+            Object.keys(result).sort(),
+            ['URL', 'date', 'nickname', 'version']
+        )
+        t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
+        Date.parse(result.date);
+        t.pass()
+        t.is(result.nickname, await get_nick(result.version));
+    }
 });
 
 test('r_release_win', async t => {
-    const result = await me.r_release_win();
-    t.deepEqual(
-        Object.keys(result).sort(),
-        ['URL', 'date', 'nickname', 'version']
-    )
-    t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
-    Date.parse(result.date);
-    t.pass()
-    t.is(result.nickname, await get_nick(result.version));
+     for (let i = 0; i < 2; i++) {
+         const result = await me.r_release_win();
+         t.deepEqual(
+             Object.keys(result).sort(),
+             ['URL', 'date', 'nickname', 'version']
+         )
+         t.true(/^[0-9]+\.[0-9]+\.[0-9]+$/.test(result.version));
+         Date.parse(result.date);
+         t.pass()
+         t.is(result.nickname, await get_nick(result.version));
+     }
 });
 
 test('get_nick', async t => {
