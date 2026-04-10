7c8629f7259e10b692fbcbc05e40be6a4515b4fa
Gábor Csárdi <csardi.gabor@gmail.com>
2026-04-10 09:02:14 +0200

    Remove old tests

--- index.js
+++ index.js
@@ -1,71 +1,44 @@
+const available = require('./lib/available');
+const resolve = require('./lib/resolve');
+const r_versions = require('./lib/r-versions');
+const r_release = require('./lib/r-release');
+const r_oldrel = require('./lib/r-oldrel');
+const r_release_macos = require('./lib/r-release-macos');
+const r_release_tarball = require('./lib/r-release-tarball');
+const r_release_win = require('./lib/r-release-win');
+const r_devel = require('./lib/r-devel');
+const r_devel_win = require('./lib/r-devel-win');
+const r_devel_macos = require('./lib/r-devel-macos');
+const r_next = require('./lib/r-next');
+const r_next_win = require('./lib/r-next-win');
+const r_next_macos = require('./lib/r-next-macos');
+const r_minor = require('./lib/r-minor');
+const rtools_versions = require('./lib/rtools').rtools_versions;
+const rtools_aarch64 = require('./lib/rtools').rtools_versions_aarch64;
+const linux_distros = require('./lib/list-linux-distros');
 
-if (!process.env.NODE_RVERSIONS_DUMMY) {
-
-    const available         = require('./lib/available');
-    const resolve           = require('./lib/resolve');
-    const r_versions        = require('./lib/r-versions');
-    const r_release         = require('./lib/r-release');
-    const r_oldrel          = require('./lib/r-oldrel');
-    const r_release_macos   = require('./lib/r-release-macos');
-    const r_release_tarball = require('./lib/r-release-tarball');
-    const r_release_win     = require('./lib/r-release-win');
-    const r_devel           = require('./lib/r-devel');
-    const r_devel_win       = require('./lib/r-devel-win');
-    const r_devel_macos     = require('./lib/r-devel-macos');
-    const r_next            = require('./lib/r-next');
-    const r_next_win        = require('./lib/r-next-win');
-    const r_next_macos      = require('./lib/r-next-macos');
-    const r_minor           = require('./lib/r-minor');
-    const rtools_versions   = require('./lib/rtools').rtools_versions;
-    const rtools_aarch64    = require('./lib/rtools').rtools_versions_aarch64;
-    const linux_distros     = require('./lib/list-linux-distros');
-
-    function get_rtools_versions(arch = "x86_64") {
-        if (arch == "x86_64") return rtools_versions;
-        if (arch == "aarch64") return rtools_aarch64;
-        throw new Error("Unsupported Windows arch: '" + arch + "'.");
-    }
-
-    module.exports = {
-        available:         available,
-        resolve:           resolve,
-        r_versions:        r_versions,
-        r_release:         r_release,
-        r_oldrel:          r_oldrel,
-        r_release_macos:   r_release_macos,
-        r_release_tarball: r_release_tarball,
-        r_release_win:     r_release_win,
-        r_devel:           r_devel,
-        r_devel_win:       r_devel_win,
-        r_devel_macos:     r_devel_macos,
-        r_next:            r_next,
-        r_next_win:        r_next_win,
-        r_next_macos:      r_next_macos,
-        r_minor:           r_minor,
-        rtools_versions:   get_rtools_versions,
-        linux_distros:     linux_distros
-    };
-
-} else {
-
-    const dummy = require('./lib/dummy');
-
-    module.exports = {
-        available:         dummy.available,
-        resolve:           dummy.resolve,
-        r_versions:        dummy.r_versions,
-        r_release:         dummy.r_release,
-        r_oldrel:          dummy.r_oldrel,
-        r_release_macos:   dummy.r_release_macos,
-        r_release_tarball: dummy.r_release_tarball,
-        r_release_win:     dummy.r_release_win,
-        r_devel:           dummy.r_devel,
-        r_devel_win:       dummy.r_devel_win,
-        r_next:            dummy.r_next,
-        r_next_win:        dummy.r_next_win,
-        r_next_macos:      dummy.r_next_macos,
-        r_minor:           dummy.r_minor,
-	rtools_versions:   dummy.rtools_versions,
-        linux_distros:     dummy.linux_distros
-    };
+function get_rtools_versions(arch = "x86_64") {
+    if (arch == "x86_64") return rtools_versions;
+    if (arch == "aarch64") return rtools_aarch64;
+    throw new Error("Unsupported Windows arch: '" + arch + "'.");
 }
+
+module.exports = {
+    available: available,
+    resolve: resolve,
+    r_versions: r_versions,
+    r_release: r_release,
+    r_oldrel: r_oldrel,
+    r_release_macos: r_release_macos,
+    r_release_tarball: r_release_tarball,
+    r_release_win: r_release_win,
+    r_devel: r_devel,
+    r_devel_win: r_devel_win,
+    r_devel_macos: r_devel_macos,
+    r_next: r_next,
+    r_next_win: r_next_win,
+    r_next_macos: r_next_macos,
+    r_minor: r_minor,
+    rtools_versions: get_rtools_versions,
+    linux_distros: linux_distros
+};
--- tests/test-dummy.js
+++
@@ -1,6 +0,0 @@
-
-var run = require('./_common.js')
-
-// Test the dummy
-process.env.NODE_RVERSIONS_DUMMY = 'true';
-run('-dummy');
